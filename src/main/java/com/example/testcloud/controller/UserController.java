6636d6092f3f123b727976dc60afc5576a27f6b6
Darlison1398 <drsilva123jd@gmail.com>
2025-01-11 23:07:30 -0300

    new atualization

--- src/main/java/com/example/testcloud/controller/UserController.java
+++ src/main/java/com/example/testcloud/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.example.testcloud.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +28,20 @@ public class UserController {
     @PostMapping("/save")
     public UserModel saveUser(@RequestBody UserModel userModel) {
         return user.save(userModel);
+    }
+
+    @GetMapping("/users")
+    public List<UserModel> getUsers(UserModel userModel) {
+        return user.findAll();
+    }
+
+    @DeleteMapping("/deleteUserById/{id}")
+    public void deleteUserById(@PathVariable Long id) {
+        try {
+            user.deleteById(id);
+        } catch (Exception e) {
+            System.out.println("Error ao excluir usuário: " + e.getMessage());
+        }
     }
     
 }
